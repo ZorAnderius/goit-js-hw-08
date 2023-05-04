e320d8fe0d7cb803ab7aa0876acac3442b290ee8
ZorAnderius <gabriel.zorander@gmail.com>
2023-05-04 18:14:17 +0100

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,34 @@
 const throttle = require('lodash.throttle');
 
+const STORAGE_KEY = 'feedback-form-state';
+
 const ref = {
   submitFormEl: document.querySelector('.feedback-form'),
   emailInputEl: document.querySelector('input[name="email"]'),
   txtAreaEl: document.querySelector('textarea[name="message"]'),
 };
+
+console.log(ref.submitFormEl);
+console.log(ref.emailInputEl);
+console.log(ref.txtAreaEl);
 let formData = {};
-let isEmpty = false;
 
 ref.submitFormEl.addEventListener('input', throttle(onInputForm, 500));
 
 function onInputForm(e) {
   const nameOfInputField = e.target.name;
+  console.log(nameOfInputField);
   const valueOfInputField = e.target.value;
+  console.log(e.target);
+  console.log(valueOfInputField);
   formData[nameOfInputField] = valueOfInputField;
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 addDataToLocalStorage();
 
 function addDataToLocalStorage() {
-  formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
   if (formData) {
     ref.emailInputEl.value = formData.email;
     ref.txtAreaEl.value = formData.message;
@@ -34,32 +42,23 @@ function onSubmitForm(e) {
   const email = ref.emailInputEl.value;
   const message = ref.txtAreaEl.value;
 
-  checkInputField(email, message);
-  if (isEmpty) {
-    isEmpty = false;
-    return;
-  }
+  // if (!email && !message) {
+  //   alert('Please enter a valid email and write a message');
+  //   return;
+  // } else if (!email) {
+  //   alert('Please enter a valid email');
+  //   return;
+  // } else if (!message) {
+  //   alert('Please write a message');
+  //   return;
+  // }
 
-  const saveData = localStorage.getItem('feedback-form-state');
+  const saveData = localStorage.getItem(STORAGE_KEY);
   const formDataSubmit = JSON.parse(saveData);
   console.log(formDataSubmit);
 
-  localStorage.removeItem('feedback-form-state');
+  localStorage.removeItem(STORAGE_KEY);
   formData = {};
 
   e.currentTarget.reset();
-}
-
-function checkInputField(email, message) {
-  isEmpty = true;
-  if (!email && !message) {
-    alert('Please enter a valid email and write a message');
-    return;
-  } else if (!email) {
-    alert('Please enter a valid email');
-    return;
-  } else if (!message) {
-    alert('Please write a message');
-    return;
-  }
 }
